ebaaa6c1a7e61b831fa2239b173c11490f84e8aa
geratarra <geratarra@gmail.com>
2023-07-02 14:22:33 -0700

    Added contact details, update and delete API operations.

--- src/api/api_handlers.clj
+++ src/api/api_handlers.clj
@@ -1,10 +1,26 @@
 (ns api.api-handlers 
-  (:require
-   [ring.util.response :refer [response]]
-   [services.contacts-service :as contacts-service :refer [add-contact]]))
+  (:require [ring.util.response :refer [bad-request response]]
+            [services.contacts-service :as contacts-service :refer [add-contact contacts
+                                                                    delete-contact edit-contact get-contacts]]))
 
 (defn contacts-handler [request]
-  (response {:contacts @contacts-service/contacts}))
+  (response {:contacts contacts}))
 
 (defn add-post-contact-handler [request]
-  (response (add-contact (:params request) contacts-service/contacts)))
+  (let [new-contact (add-contact (:params request) contacts-service/contacts)]
+    (if (:error new-contact)
+      (bad-request new-contact)
+      (response new-contact))))
+
+(defn contact-details-handler [request]
+  (response (get-contacts :id  (Integer/parseInt (get-in request [:params :id])) @contacts)))
+
+(defn update-contact-handler [request]
+  (let [contact (edit-contact (assoc (:params request) :id (Integer/parseInt (get-in request [:params :id]))) contacts)]
+    (if (:error contact)
+      (bad-request contact)
+      (response contact))))
+
+(defn delete-contact-handler [request]
+  (delete-contact (assoc (:params request) :id (Integer/parseInt (get-in request [:params :id]))) contacts)
+  (response {:success true}))
--- src/api/api_routes.clj
+++ src/api/api_routes.clj
@@ -1,7 +1,10 @@
 (ns api.api-routes 
   (:require [api.api-handlers :as api-handlers]
-            [compojure.core :refer [defroutes GET POST]]))
+            [compojure.core :refer [defroutes DELETE GET POST PUT]]))
 
 (defroutes api-routes
+  (GET "/api/v1/contacts/:id" request api-handlers/contact-details-handler)
+  (PUT "/api/v1/contacts/:id" request api-handlers/update-contact-handler)
+  (DELETE "/api/v1/contacts/:id" request api-handlers/delete-contact-handler)
   (GET "/api/v1/contacts" request api-handlers/contacts-handler)
   (POST "/api/v1/contacts" request api-handlers/add-post-contact-handler))
