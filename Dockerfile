967fc66c7151f37cb881f02d65ed46f772217585
geratarra <geratarra@gmail.com>
2023-07-15 17:35:07 -0700

    Replaced amazon correto alpine distribution with a normal amazon correto one. Added health-check route for AWS.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM amazoncorretto:17-alpine-jdk
+FROM amazoncorretto:17
 RUN mkdir -p /app /app/resources
 WORKDIR /app
+HEALTHCHECK --interval=1m --timeout=3s \
+CMD curl -f http://localhost/api/v1/health/ || exit 1
 COPY target/*-standalone.jar .
 COPY resources/public resources/public
-CMD java -jar *-standalone.jar
-EXPOSE 3000
+EXPOSE 8080
+ENTRYPOINT [ "java", "-jar", "hello-ring-standalone.jar"]
--- src/api/api_routes.clj
+++ src/api/api_routes.clj
@@ -1,8 +1,10 @@
 (ns api.api-routes 
   (:require [api.api-handlers :as api-handlers]
-            [compojure.core :refer [defroutes DELETE GET POST PUT]]))
+            [compojure.core :refer [defroutes DELETE GET POST PUT]]
+            [ring.util.response :as response]))
 
 (defroutes api-routes
+  (GET "/api/v1/health" [] (response/content-type (response/response "Health check succeeded") "text/html; charset=utf-8"))
   (GET "/api/v1/contacts/:id" request api-handlers/contact-details-handler)
   (PUT "/api/v1/contacts/:id" request api-handlers/update-contact-handler)
   (DELETE "/api/v1/contacts/:id" request api-handlers/delete-contact-handler)
